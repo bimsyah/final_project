06337e447ba9b6327236d1e0be95415c3d7b9618
Bima Syahputra <macbookpro@Macbooks-MacBook-Pro.local>
2020-06-05 16:55:46 +0700

    projectFinal

---
+++ medium.js
@@ -0,0 +1,62 @@
+const tong = document.querySelectorAll('.tongsampah');
+const lalat = document.querySelectorAll('.lalat');
+const papanSkor = document.querySelector('.papanSkor')
+const pok = document.querySelector('#pok')
+const music = document.querySelector('#music')
+
+let tongSebelumnya;
+let selesai;
+let skor;
+
+
+function randomTong(tong) {
+    const t = Math.floor(Math.random() * tong.length)
+    const tRandom = tong[t]
+    if (tRandom == tongSebelumnya){
+        randomTong(tong)
+    }
+    tongSebelumnya = tRandom
+    return tRandom
+}
+
+function randomWaktu (start, end){
+    return Math.round(Math.random() * (start - end) + end);
+}
+
+function munculkanLalat() {
+    const tRandom = randomTong(tong)
+    const wRandom = randomWaktu(800, 1300);
+    tRandom.classList.add('muncul')
+
+    setTimeout(() => {
+        tRandom.classList.remove('muncul')  
+        if(!selesai){
+           munculkanLalat() 
+        }
+        
+    }, wRandom);
+}
+
+function mulai() {
+    selesai = false
+    skor = 0
+    munculkanLalat()
+    music.play()
+    setTimeout(() => {
+        selesai = true
+        alert(`Selesai! Selamat anda sudah menyelesaikan permainan! dan skor anda adalah ${skor}`)
+        skor.textContent= 0
+    }, 10000);
+    
+}
+
+function tepok() {
+    skor++
+    this.parentNode.classList.remove('muncul')
+    pok.play()
+    papanSkor.textContent = skor
+}
+
+lalat.forEach(t => {
+    t.addEventListener('click', tepok)
+})
